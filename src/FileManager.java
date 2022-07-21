7b8f0d7189c7d567002aa96e6bcf1661a612650e
Konrad THC <konradgrb.1@GMAIL.com>
2022-07-21 20:59:16 +0200

    throwing exceptions

--- src/FileManager.java
+++ src/FileManager.java
@@ -2,34 +2,40 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.*;
+import java.util.function.BooleanSupplier;
 
 public class FileManager {
 
 
-
-    private Map<Integer, List<String>> mapOfWords = new HashMap<>();
+    private static Map<Integer, List<String>> mapOfWords = new HashMap<>();
 
     public Map<Integer, List<String>> getMapOfWords() {
         return mapOfWords;
     }
 
     public void importPasswords(String path) throws FileNotFoundException {
+        String delimiter = userRegexInput();
         File file = new File(path);
         Scanner sc = new Scanner(file);
-        int i = 0;
+        List<String> lines = new ArrayList<>();
         while (sc.hasNextLine()) {
-            mapOfWords.put(i, Arrays.asList((sc.nextLine().split(", "))));
-            i++;
-
+            lines.add((sc.nextLine()));
         }
-
+        boolean check = isDelimiterPresent(delimiter, lines);
+        if(check == true){
+            addWordsToMap(delimiter, lines);
+        } else{
+            System.out.println("Sorry! Your delimiter isn't there :/ ");
+            importPasswords(path);
+        }
     }
+
     public void exportPassword(List<String> listOfPasswords) {
         try {
 
             PrintWriter out = new PrintWriter("finalPassword.txt");
             out.println("Your passwords generated with using of random words is: ");
-            for(String password : listOfPasswords) {
+            for (String password : listOfPasswords) {
                 out.println(password);
             }
             out.println("don't show it to anyone ;)))) ");
@@ -37,8 +43,32 @@ public class FileManager {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
+    public static Boolean isDelimiterPresent(String delimiter, List<String> lines) {
+        for (String line : lines) {
+            if (!line.contains(delimiter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void addWordsToMap(String delimiter, List<String> lines){
+        for(int i = 0; i<lines.size(); i++){
+            mapOfWords.put(i, Arrays.asList(lines.get(i).split(delimiter)));
+        }
+    }
+    public static String userRegexInput(){
+        System.out.println("Input delimiter used in file: ");
+        String regex = "";
+        try {
+            Scanner regexInput = new Scanner(System.in);
+            regex = regexInput.nextLine();
+            regexInput.close();
+
+        }catch (NoSuchElementException ex){
+            System.out.println(ex);
+        }
+        return regex;
+    }
 }
--- src/StartPasswordGenerator.java
+++ src/StartPasswordGenerator.java
@@ -1,15 +1,19 @@
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class Main {
+public class StartPasswordGenerator {
 
     public static void main(String[] args) throws FileNotFoundException {
-        FileManager fileManager = new FileManager();
+      FileManager fileManager = new FileManager();
         PasswordGenerator genPass = new PasswordGenerator();
         fileManager.importPasswords("C:\\Users\\Admin\\Desktop\\PasswordGenerator\\words.txt");
         List<String> finalOutcome = genPass.generateMultiplePasswords(fileManager.getMapOfWords());
         fileManager.exportPassword(finalOutcome);
     }
+
+
+
+
 
 }
